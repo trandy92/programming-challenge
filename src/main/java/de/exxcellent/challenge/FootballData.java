fd8d8262e4c1794eac38840eb47b8b831c020044
andreas.reichel <andreas.reichel@sovanta.com>
2021-01-30 10:33:25 +0100

    Added Football classes with tests.

---
+++ src/main/java/de/exxcellent/challenge/FootballData.java
@@ -0,0 +1,57 @@
+package de.exxcellent.challenge;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class FootballData {
+    List<Map<String, String>> footballData = new ArrayList<>();
+    private final static Set<String> EXPECTED_HEADER_COLUMNS =Set.<String>of("Team","Games","Wins","Losses","Draws","Goals","Goals Allowed","Points");
+
+    public FootballData(DataFactory dataFactory) throws IOException {
+        footballData = dataFactory.getData();
+
+        if(isValidFormat() == false)
+        {
+            throw new InvalidWeatherDataException("Format of weather data is incorrect");
+        }
+    }
+
+    private boolean isValidFormat()
+    {
+        try {
+            //Maybe not a good idea to check every data entry for valid format
+            for(Map<String,String> footballDataEntry : footballData) {
+                for (Map.Entry<String, String> entry : footballDataEntry.entrySet()) {
+                    if (!entry.getKey().equals("Team")) {
+                        Integer.parseInt(entry.getValue());
+                    }
+                }
+            }
+        }catch(Exception ex)
+        {
+            return false;
+        }
+
+        Set<String> headerColumns = footballData.get(0).keySet();
+        return EXPECTED_HEADER_COLUMNS.equals(headerColumns);
+    }
+
+    public int getNumberEntries() {
+        return footballData.size();
+    }
+
+    public String getTeam(int index)
+    {
+        return footballData.get(index).get("Team");
+    }
+
+    public String getTeamWithSmallestDifferenceBetweenGoalsAndGoalsAllowed()
+    {
+        int index = DataAnalyser.getEntryWithSmallestSpread(footballData,"Goals", "Goals Allowed");
+
+        return getTeam(index);
+    }
+}
---
+++ src/test/java/de/exxcellent/challenge/FootballDataTest.java
@@ -0,0 +1,21 @@
+package de.exxcellent.challenge;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FootballDataTest {
+    @Test
+    void getFootballDataFromCsv() throws IOException {
+        File csv = TestUtils.getTestCSVFile("Team,Games,Wins,Losses,Draws,Goals,Goals Allowed,Points\nArsenal,38,26,9,3,79,36,87\nLiverpool,38,24,8,6,67,30,80\nManchester United,38,24,5,9,87,45,77");
+
+        FootballData footballData = new FootballData(new CSVDataFactory(csv));
+
+        assertEquals( 3, footballData.getNumberEntries());
+        assertEquals("Liverpool", footballData.getTeamWithSmallestDifferenceBetweenGoalsAndGoalsAllowed());
+        assertEquals("Arsenal", footballData.getTeam(0));
+    }
+}
